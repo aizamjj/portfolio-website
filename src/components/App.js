6b53b64af31b24c18a5ea2723a31ae472a3a9075
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-07-02 18:44:30 -0700

    Complete set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './App.scss';
+
+// component imports
+import Image from './Image/Image';
+
+// other imports
+import profile from '../../public/images/profile.jpg';
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <span>Hello World</span>
+      <Image 
+        src={profile}
+        alt="hero"
+        height="400"
+        width="400"
+      />
+    </div>
   );
 };
 
--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,12 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
     ],
   },
   plugins: [
