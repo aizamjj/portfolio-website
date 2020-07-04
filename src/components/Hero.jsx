aa1765eac6e0c1a8371fe0eb154e92d0e7439386
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-07-04 14:38:46 -0700

    Style Hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,36 @@
 import React, { useContext } from 'react';
 import PortfolioContext from '../context/context';
+import { withStyles } from '@material-ui/core/styles';
+
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+
 
 const Hero = () => {
   const { hero } = useContext(PortfolioContext);
   const { title, name, subtitle, cta } = hero;
 
+  const StyledButton = withStyles({
+    root: {
+      background: 'linear-gradient(45deg, #00b09b 10%, #96c93d 100%)',
+    }
+
+  })(Button);
+
   return (
       <section id="hero">
         <Container>
-          <h1>
-            {title}
-            <span>
-                {name}
+            <h1 className="hero-title">
+              {title}
+              <span className="hero-name">{name}</span>
                 <br />
                 {subtitle}
-            </span>
-          </h1>
-          <p>
-            <a>
-              {cta}
-            </a>
-          </p>
+            </h1>
+            <p className="hero-cta">
+              <StyledButton>
+                {cta}
+              </StyledButton>
+            </p>
         </Container>
       </section>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './style/main.scss';
 
 const $container = document.getElementById('app');
 $container ? ReactDOM.render(<App />, $container) : false;
