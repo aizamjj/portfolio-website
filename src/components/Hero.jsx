d10cbed32992823b021d64eb8b3c52f74a45f5e6
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-07-07 17:21:54 -0700

    Add slide transition for hero.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,48 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, config, animated } from 'react-spring';
 import PortfolioContext from '../context/context';
 
 const Hero = () => {
   const { hero } = useContext(PortfolioContext);
   const { title, name, subtitle, cta } = hero;
-  const letters = ['a', 'j', ' ', 'j'];
 
-  //Animation
-  
-  
   const StyledButton = withStyles({
     root: {
       background: 'linear-gradient(45deg, #00b09b 10%, #96c93d 100%)',
     }
-    
   })(Button);
-      const props = useSpring({
-        opacity: 1,
-        from: { opacity: 0 },
-      });
-    
-      const AnimatedView = animated(StyledButton);
+
+  const props = useSpring({
+    from: {
+      opacity: 0, 
+      marginLeft: -50,
+    },
+    to: {
+      opacity: 1,
+      marginLeft: 0,
+    },
+    config: config.slow,
+  });
 
   return (
       <section id="hero">
         <Container>
+          <animated.div style={props}>
             <h1 className="hero-title">
               {title}
-              <animated.span className="hero-name" style={props}>{name}</animated.span>
+              <span className="hero-name">{name}</span>
                 <br />
                 {subtitle}
             </h1>
-            <p className="hero-cta">
-            <AnimatedView style={props}>
+          </animated.div>
+            <animated.p className="hero-cta" style={ props } >
+              <StyledButton>
                 {cta}
-              </AnimatedView>
-              {/* <StyledButton>
-                {cta}
-              </StyledButton> */}
-            </p>
+              </StyledButton>
+            </animated.p>
         </Container>
       </section>
   );
