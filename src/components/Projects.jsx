487889722bfc7b05f7aa179db7aa91e78bf32f4b
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-07-20 21:45:55 -0700

    Style projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,25 +3,26 @@ import PortfolioContext from '../context/context';
 import Title from './Title';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container'
 
 const Projects = () => {
   const { projects } = useContext(PortfolioContext);
 
   return (
     <section id="projects">
-        <div>
+        <Container>
           {projects.map((projects, index) => {
             const { name, url, description } = projects;
             return (
             <div className="box">
                 <div className="content">
-                  <h2 key={index} className='projects-name' >{name}</h2>
-                  <p key={index} className='projects-description' >{description}</p>
+                  <h2 key={index} className="projects-name" >{name}</h2>
+                  <p key={index} className="projects-description" >{description}</p>
                 </div>
             </div>
             )
           })}
-        </div>
+        </Container>
     </section>
   )
 }
